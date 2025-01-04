d887df1d145eececabb5e6f840df2336efcdfca4
Vedschyburger <48015575+Vedschyburger@users.noreply.github.com>
2025-01-04 19:50:46 +0100

    Update Vedschyburger_TetherPaw.lua.lua

--- Vedschyburger_TetherPaw.lua.lua
+++ Vedschyburger_TetherPaw.lua.lua
@@ -1,10 +1,17 @@
--- This code implements a command in FiveM (GTA V multiplayer mod) that allows a player character to be “leashed” to another player using a rope. 
+-- Copyright (c) 2025, Vedschyburger
+-- All rights reserved.
+-- My Github: https://github.com/Vedschyburger
+-- 
+-- This code may be freely used, modified and distributed as long as the above copyright and license text remains unchanged and the main owner (Vedschy) is explicitly mentioned in the modified code. 
+-- Any changes to the code must be clearly documented.
+-- 
+-- This code is provided “as is”. No liability is accepted for any damage or loss of data resulting from the use of this code.
+
+-- This code implements a command in FiveM (GTA V Multiplayer Mod) that allows a player character to be “tethered” to another player using a rope. 
 -- When another player is within a certain range, the player can create a tether and attach it to the target player. 
 -- The leash is displayed visually and the player is attached to the target player with a rope animation. 
 -- The command is primarily used for role-playing scenarios in which, for example, dogs are leashed to their owners. 
 -- The code also contains a function for removing the leash if it is already active.
--- Creator and copyright by @Vedschyburger
--- My Github: https://github.com/Vedschyburger
 
 local leashed = false  -- Tracks if the player is currently leashed
 local targetPlayer = nil  -- Stores the player being leashed
